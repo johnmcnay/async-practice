48bec410dcc881861465f1474212ac997c21b54a
John McNay <John.McNay@improving.com>
2021-06-04 08:53:01 -0500

    async await for the three API calls

--- async-practice-thing/Controllers/HomeController.cs
+++ async-practice-thing/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using async_practice_thing.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,8 @@ namespace async_practice_thing.Controllers
 {
     public class HomeController : Controller
     {
+
+        private static readonly HttpClient HttpClient;
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -22,6 +25,40 @@ namespace async_practice_thing.Controllers
         {
             return View();
         }
+
+        public async Task<int> RandomNumber()
+        {
+
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://seriouslyfundata.azurewebsites.net/api/generatearandomnumber");
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return Convert.ToInt32(responseString);
+        }
+
+        public async Task<string> ChuckNorris()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://seriouslyfundata.azurewebsites.net/api/chucknorrisfact");
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return responseString;
+
+        }
+
+        public async Task<string> Seleucids()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://seriouslyfundata.azurewebsites.net/api/seleucids");
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return responseString;
+
+        }
+
 
         public IActionResult Privacy()
         {
