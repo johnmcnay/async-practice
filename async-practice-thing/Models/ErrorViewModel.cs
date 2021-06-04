0df89b43d146a92c6674ac795e8ae3283d4cd90c
John McNay <John.McNay@improving.com>
2021-06-04 08:03:29 -0500

    initial commit

---
+++ async-practice-thing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace async_practice_thing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
